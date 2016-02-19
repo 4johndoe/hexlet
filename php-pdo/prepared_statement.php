14e01ee20e768eb0818beb1f825580db6bd74c24
bogdan demchenko <bogdandemchenko@gmail.com>
2016-02-19 09:05:54 +0200

    prepared

--- php-pdo/prepared_statement.php
+++ php-pdo/prepared_statement.php
@@ -1,9 +1,15 @@
 <?php
-$pdo = new \PDO();
-// 
-$stmt = $pdo->prepare("SELECT name FROM users WHERE role = ? AND name != ?");
-$stmt->execute(['member', 'john']);
-//
-$stmt = $pdo->prepare("SELECT name FROM users WHERE role = :role");
-$stmt->bindValue(':role', 'member', \PDO::PARAM_STR);
-$stmt->execute();
+// $pdo = new \PDO();
+// //
+// $stmt = $pdo->prepare("SELECT name FROM users WHERE role = ? AND name != ?");
+// $stmt->execute(['member', 'john']);
+// // binder
+// $stmt = $pdo->prepare("SELECT name FROM users WHERE role = :role");
+// $stmt->bindValue(':role', 'member', \PDO::PARAM_STR);
+// $stmt->execute();
+
+$stmt = $this->pdo->prepare("INSERT INTO user_photos VALUES (?, ?)");
+
+foreach ($user->getPhotos() as $photo) {
+  $stmt->execute([$photo->getName(), $photo->getFilepath()]);
+}
